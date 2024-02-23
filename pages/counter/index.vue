5e2bf0d9f97349f7bfde04e931bb62c7744a571a
Fumito Inoue <fumito.inoue@gmail.com>
2024-02-24 03:11:52 +0900

    add validator

--- pages/counter/index.vue
+++ pages/counter/index.vue
@@ -1,11 +1,14 @@
 <template>
   <VContainer fluid class="fill-height d-flex justify-center">
-    <VCard>
+    <VCard class="w-25">
       <VCardTitle class="headline">Counter</VCardTitle>
       <VCardSubtitle>Simple counter example</VCardSubtitle>
       <VCardText>
         <h1>{{ counter.count }}</h1>
         <pre>{{ test }}</pre>
+
+        <VTextField v-model="value" :error-messages="errorMessage" label="Email" />
+
       </VCardText>
       <VCardActions>
         <VBtn @click="counter.increment" type="submit" class="gradient primary">Increment</VBtn>
@@ -17,8 +20,15 @@
 
 <script setup>
 import { useCounterStore } from "@/composables/counter";
+import { useField } from 'vee-validate';
+import * as yup from 'yup';
 
-//const { data: test } = useFetch("https://api.github.com/users/posva");
+// validation
+const { value, errorMessage } = useField("sample",
+  yup.string().required("この項目は必須です").email("メールアドレスの形式で入力してください"));
+
+
+// fetch data
 const { data: test } = useFetch("/api/hello");
 const counter = useCounterStore();
 </script>
