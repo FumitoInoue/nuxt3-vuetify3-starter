025f94513d14a06f58567ef22b47c54f5c1e08a4
Fumito Inoue <fumito.inoue@gmail.com>
2024-02-24 00:18:36 +0900

    server apiを追加

--- pages/counter/index.vue
+++ pages/counter/index.vue
@@ -5,6 +5,7 @@
       <VCardSubtitle>Simple counter example</VCardSubtitle>
       <VCardText>
         <h1>{{ counter.count }}</h1>
+        <pre>{{ test }}</pre>
       </VCardText>
       <VCardActions>
         <VBtn @click="counter.increment" type="submit" class="gradient primary">Increment</VBtn>
@@ -17,5 +18,7 @@
 <script setup>
 import { useCounterStore } from "@/composables/counter";
 
+//const { data: test } = useFetch("https://api.github.com/users/posva");
+const { data: test } = useFetch("/api/hello");
 const counter = useCounterStore();
 </script>
